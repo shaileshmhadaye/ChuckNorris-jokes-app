f68e8e640f31a82e46d66b77d9c9d086d63c6b5d
shaileshmhadaye <shaileshmhadaye2@gmail.com>
2019-09-10 12:02:47 +0530

    Final implementations

--- src/main/java/com/neo/demo/controllers/JokeController.java
+++ src/main/java/com/neo/demo/controllers/JokeController.java
@@ -15,7 +15,7 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping("")
+    @RequestMapping("/")
     public String jokes(Model model){
         model.addAttribute("joke",jokeService.getJoke());
         return "chucknorris";
