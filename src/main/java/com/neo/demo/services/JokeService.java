8d21e37d99d034f257448907d4ed3b9e827b0598
shaileshmhadaye <shaileshmhadaye2@gmail.com>
2019-09-10 11:52:20 +0530

    Initial commit

---
+++ src/main/java/com/neo/demo/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.neo.demo.services;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/com/neo/demo/services/JokeServiceImpl.java
@@ -0,0 +1,12 @@
+package com.neo.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService{
+
+    @Override
+    public String getJoke() {
+        return null;
+    }
+}
